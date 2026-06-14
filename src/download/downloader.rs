da4324ee04bb3bd4ba9ad6c1f270e73ec4c19435
ggjorven <jorbenvanderwal@gmail.com>
2026-06-14 23:48:45 +0200

    Fix downloader.

--- src/download/downloader.rs
+++ src/download/downloader.rs
@@ -7,7 +7,9 @@ use url::Url;
 use tokio::fs::OpenOptions;
 
 use crate::config;
+use crate::download::m3u::M3UResult;
 use crate::request;
+use crate::request::HeaderMap;
 use crate::search::streams::Stream;
 
 use super::m3u;
@@ -17,13 +19,16 @@ use super::m3u;
 /////////////////////////////////////////////////////
 #[derive(Debug, Error)]
 pub enum DownloadError {
-    #[error("Indexer download type doesn't match stream type passed in")]
-    InvalidStreamIndexerCombo,
-
+    #[error("Failed to get stream from \"{0}\" due to error: {1}")]
+    FailedToGetStream(Url, request::RequestError),
+    #[error("Failed to parse stream due to error: {0}")]
+    FailedToParseStream(#[from] m3u::ParseError),
+    #[error("M3U does not follow expectations: {0}")]
+    InvalidM3U(String),
     #[error("Failed to open output file \"{file}\" with error: {error}", file = file.display())]
     FailedToOpenOutputFile { file: PathBuf, error: String },
     #[error("Request error: {0}")]
-    RequestFailed(request::RequestError),
+    RequestFailed(#[from] request::RequestError),
     #[error("Failed to write bytes to disk due to error: {0}")]
     FailedToWriteBytes(String),
 }
@@ -34,6 +39,28 @@ pub enum DownloadError {
 pub async fn download_stream(
     indexer: &config::Indexer, stream: Stream, requester: &request::Requester, output_file: &Path,
 ) -> Result<(), DownloadError> {
+    trace!("Downloading stream of resolution: {} from \"{}\".", stream.quality, stream.url);
+
+    // Parse stream
+    // TODO: Handle non-m3u
+    let m3u = requester
+        .get_string(&stream.url, Some(indexer.download.segment_download.headers.clone()))
+        .await
+        .map_err(|error| DownloadError::FailedToGetStream(stream.url.clone(), error))?;
+    let result = m3u::parse_m3u_contents(m3u.as_str())?;
+    let M3UResult::Index(segments) = result else {
+        error!("Unable to download requested stream since the m3u is not a index.m3u(8) file.");
+        return Err(DownloadError::InvalidM3U("The m3u file is not a index.m3u(8) file.".to_string()));
+    };
+    let segments: Vec<Url> = segments
+        .iter()
+        .map(|segment| Url::parse(segment))
+        .collect::<Result<Vec<_>, _>>()
+        .map_err(|_error| {
+            error!("A url in the index.m3u(8) is not a valid url.");
+            return DownloadError::InvalidM3U("One of the urls in the index.m3u(8) file is broken or does not follow the url standard.".to_string());
+        })?;
+
     // Open output
     trace!("Opening file \"{}\" for writing...", output_file.display());
 
@@ -49,7 +76,5 @@ pub async fn download_stream(
     trace!("File \"{}\" successfully opened.", output_file.display());
 
     // Download based of of stream_type
-    match stream.stream_type {
-        StreamType::M3U(segments) => m3u::download_segments(indexer, segments, requester, &mut file).await,
-    }
+    m3u::download_segments(indexer, segments, requester, &mut file).await
 }
